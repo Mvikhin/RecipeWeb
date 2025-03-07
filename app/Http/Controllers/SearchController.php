e7fce0f4cc68f156e5186405da4ec4ae3c4dc851
Arisuzea <grldcondezz18@gmail.com>
2025-03-07 20:10:43 +0800

    Optimized and integrated functional search

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Recipe;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function search(Request $request)
+    {
+        $query = $request->get('query');
+        
+        $results = Recipe::where('name', 'LIKE', "%{$query}%")
+            ->orWhere('title', 'LIKE', "%{$query}%")
+            ->orWhere('description', 'LIKE', "%{$query}%")
+            ->select('id', 'name', 'title', 'description')
+            ->limit(10)
+            ->get();
+            
+        return response()->json($results);
+    }
+}
